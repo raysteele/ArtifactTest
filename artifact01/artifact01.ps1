d1f5dd0fd8666442ef054c009224e04627c16a94
raysteele <rsteele@wsipc.org>
2017-09-01 08:44:56 -0700

    testing again

--- artifact01/artifact01.ps1
+++ artifact01/artifact01.ps1
@@ -3,5 +3,5 @@ param(
     [string] $StringParam2
 )
 
-$StringParam1 | out-file C:\Logs\newfile-Artifact02.txt
-$StringParam2 | out-file C:\Logs\newfile-Artifact02.txt -Append
+$StringParam1 | out-file C:\Logs\newfile-Artifact01.txt
+$StringParam2 | out-file C:\Logs\newfile-Artifact01.txt -Append
