c30fc9d4ed7fbb3949c1c51542ab92ca1bde88eb
raysteele <rsteele@wsipc.org>
2017-09-01 14:05:12 -0700

    More testing on artifact01

--- artifact01/runme.ps1
+++
@@ -1,7 +0,0 @@
-param(
-    [string] $StringParam1,
-    [string] $StringParam2
-)
-
-$StringParam1 | out-file C:\Logs\newfile-Artifact01.txt
-$StringParam2 | out-file C:\Logs\newfile-Artifact01.txt -Append
