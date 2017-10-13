4a7d82a0b4425e39d491e8d3aea27fe1ade43538
raysteele <rsteele@wsipc.org>
2017-10-13 15:52:18 -0700

    Update SQLRestore.ps1

--- SQLRestore/SQLRestore.ps1
+++ SQLRestore/SQLRestore.ps1
@@ -26,11 +26,8 @@ Stop-Transcript
 
 WF-Skyward.zip
 \\samgtstd.file.core.windows.net\release
-13HMez1ltwux48kcjoILtMUT3mBEBFy7gmS67CWIXFAFmv/dJc5a7D0yZrMyCsDdvz7E8Ps3ogzDesejCqJsbw==
+13H
 
-13HMez1ltwux48kcjoILtMUT3mBEBFy7gmS67CWIXFAFmv/dJc5a7D0yZrMyCsDdvz7E8Ps3ogzDesejCqJsbw==
-
-net use \\samgtstd.file.core.windows.net\release /u:AZURE\samgtstd 13HMez1ltwux48kcjoILtMUT3mBEBFy7gmS67CWIXFAFmv/dJc5a7D0yZrMyCsDdvz7E8Ps3ogzDesejCqJsbw==
-
+net use \\samgtstd.file.core.windows.net\release /u:AZURE\samgtstd 1
 
 Restore-SqlDatabase -ServerInstance "TestSQL\Instance" -Database "Qmlative" -BackupFile "https://samgtstd.blob.core.windows.net/backups/WSIPC_OceanBeach.bak"
